1a4b2617250871963fd6123a0b8d85ea0a669ae4
imerofeev <imerofeev@gmail.com>
2020-07-14 07:32:21 +0300

    add result calculation and student list

--- js/main.js
+++ js/main.js
@@ -1,198 +1,216 @@
 $(document).ready(function () {
-  $("#btnSubmit").on("click", function () {
-    if (hasEmptyRequiredInput()) {
-      $("#alert").modal("show");
-      return false;
-    }
-    Get_scores();
-    return false;
-  });
+	$("#btnSubmit").on("click", function () {
+		if (hasEmptyRequiredInput()) {
+			$("#alert").modal("show");
+			return false;
+		}
+		Get_scores();
+		return false;
+	});
+
+	var studentClassInput = $("#studentClass");
+	var studentSelect = $("#studentName");
+	var studentIdInput = $("#studentNumber");
+
+	studentSelect.on("change", function (e) {
+		var classNum = studentClassInput[0].value;
+		studentIdInput[0].value = students[classNum][e.target.value];
+	});
+
+	studentClassInput.on("change", function (e) {
+		switch (e.target.value) {
+			case "1":
+				addStudentsToSelect(studentSelect, students[1]);
+				break;
+			case "2":
+				addStudentsToSelect(studentSelect, students[2]);
+				break;
+			case "3":
+				addStudentsToSelect(studentSelect, students[3]);
+				break;
+			default:
+				addStudentsToSelect(studentSelect, []);
+				break;
+		}
+	});
 });
 
-function inputsInformation(inputs) {
-  var text = "";
-  for (var i = 0; i < inputs.length; i++) {
-    var input = inputs[i];
-    var element = $("#" + input.id);
+function addStudentsToSelect(select, students) {
+	select.html("<option></option>");
+	var studentsName = Object.keys(students);
+	for (var i = 0; i < studentsName.length; i++) {
+		var option = document.createElement("option");
+		option.innerText = studentsName[i];
+		select.append(option);
+	}
+}
 
-    if (element && _.isEmpty(element.val())) {
-      $("#" + input.divId).addClass("has-error");
-      text += input.text + "";
-    } else {
-      $("#" + input.divId).removeClass("has-error");
-    }
-  }
-  return text;
+function inputsInformation(inputs) {
+	var text = "";
+	for (var i = 0; i < inputs.length; i++) {
+		var input = inputs[i];
+		var element = $("#" + input.id);
+
+		if (element && _.isEmpty(element.val())) {
+			$("#" + input.divId).addClass("has-error");
+			text += input.text + "";
+		} else {
+			$("#" + input.divId).removeClass("has-error");
+		}
+	}
+	return text;
 }
 
 function hasEmptyRequiredInput() {
-  var requiredInputs = [
-    {
-      id: "studentClass",
-      text: "class",
-      divId: "class",
-    },
-    {
-      id: "studentNumber",
-      text: "number",
-      divId: "number",
-    },
-    {
-      id: "studentName",
-      text: "name",
-      divId: "name",
-    },
-  ];
+	var requiredInputs = [
+		{
+			id: "studentClass",
+			text: "class",
+			divId: "class",
+		},
+		{
+			id: "studentNumber",
+			text: "number",
+			divId: "number",
+		},
+		{
+			id: "studentName",
+			text: "name",
+			divId: "name",
+		},
+	];
 
-  var text = inputsInformation(requiredInputs);
-  if (text !== "") {
-    return true;
-  }
-  return false;
+	var text = inputsInformation(requiredInputs);
+	if (text !== "") {
+		return true;
+	}
+	return false;
 }
 
 function Get_scores() {
-  var value =
-    fullInTopics() +
-    choiceTopics() +
-    multipleChoiceTopics() +
-    trueOrFalseTopics() +
-    shortAnswerTopics();
-  $("#scores").text(value);
-  $("#divScores").addClass("text-danger");
+	var value = fullInTopics() + choiceTopics() + multipleChoiceTopics() + trueOrFalseTopics() + shortAnswerTopics();
+	$("#scores").text(value);
+	$("#result").text(calcResult(value));
+	$("#divScores").addClass("text-danger");
+	$("#divResult").addClass("text-danger");
+}
+
+function calcResult(value) {
+	if (value > 100) return "5";
+	else if (value > 84) return "4";
+	else if (value > 60) return "3";
+	else return "2";
 }
 
 function fullInTopics() {
-  var fullInSubject1 = new Subject(
-    "fullInSubject",
-    ["Unified Modeling Language"],
-    1,
-    5
-  );
-  var fullInSubject2 = new Subject(
-    "fullInSubject",
-    ["Inheritance", "Polymorphism", "Encapsulation"],
-    3,
-    5
-  );
+	var fullInSubject1 = new Subject("fullInSubject", ["Unified Modeling Language"], 1, 5);
+	var fullInSubject2 = new Subject("fullInSubject", ["Inheritance", "Polymorphism", "Encapsulation"], 3, 5);
 
-  var value1_1_1 = $("#gap1").val();
+	var value1_1_1 = $("#gap1").val();
 
-  if (value1_1_1 == fullInSubject1.answer[0]) {
-    fullInSubject1.scores += fullInSubject1.scorePerSubject;
-  }
+	if (value1_1_1 == fullInSubject1.answer[0]) {
+		fullInSubject1.scores += fullInSubject1.scorePerSubject;
+	}
 
-  var value1_2 = [];
-  value1_2.push($("#gap2_1").val());
-  value1_2.push($("#gap2_2").val());
-  value1_2.push($("#gap2_3").val());
+	var value1_2 = [];
+	value1_2.push($("#gap2_1").val());
+	value1_2.push($("#gap2_2").val());
+	value1_2.push($("#gap2_3").val());
 
-  for (var i = 0; i < fullInSubject2.answer.length; i++) {
-    for (var j = 0; j < value1_2.length; j++) {
-      if (fullInSubject2.answer[i] == value1_2[j]) {
-        fullInSubject2.scores += fullInSubject2.scorePerSubject;
-        break;
-      }
-    }
-  }
-  return fullInSubject1.scores + fullInSubject2.scores;
+	for (var i = 0; i < fullInSubject2.answer.length; i++) {
+		for (var j = 0; j < value1_2.length; j++) {
+			if (fullInSubject2.answer[i] == value1_2[j]) {
+				fullInSubject2.scores += fullInSubject2.scorePerSubject;
+				break;
+			}
+		}
+	}
+	return fullInSubject1.scores + fullInSubject2.scores;
 }
 
 function choiceTopics() {
-  var choiceSubject = new Subject("choiceSubject", ["B", "A"], 2, 10);
+	var choiceSubject = new Subject("choiceSubject", ["B", "A"], 2, 10);
 
-  var choiceSubject1 = new ChoiceSubject("radio_ans_1");
-  var value1 = choiceSubject1.calculation();
-  var choiceSubject2 = new ChoiceSubject("radio_ans_2");
-  var value2 = choiceSubject2.calculation();
-  var value = [value1, value2];
+	var choiceSubject1 = new ChoiceSubject("radio_ans_1");
+	var value1 = choiceSubject1.calculation();
+	var choiceSubject2 = new ChoiceSubject("radio_ans_2");
+	var value2 = choiceSubject2.calculation();
+	var value = [value1, value2];
 
-  for (var i = 0; i < value.length; i++) {
-    if (value[i] == choiceSubject.answer[i]) {
-      choiceSubject.scores += choiceSubject.scorePerSubject;
-    }
-  }
-  return choiceSubject.scores;
+	for (var i = 0; i < value.length; i++) {
+		if (value[i] == choiceSubject.answer[i]) {
+			choiceSubject.scores += choiceSubject.scorePerSubject;
+		}
+	}
+	return choiceSubject.scores;
 }
 
 function multipleChoiceTopics() {
-  var multipleChoiceSubject = new Subject(
-    "multipleChoiceSubject",
-    [
-      ["A", "B", "D"],
-      ["A", "B", "C"],
-      ["A", "B", "D"],
-    ],
-    2,
-    10
-  );
+	var multipleChoiceSubject = new Subject(
+		"multipleChoiceSubject",
+		[
+			["A", "B", "D"],
+			["A", "B", "C"],
+			["A", "B", "D"],
+		], 2, 10);
 
-  var multipleChoiceSubject1 = new MultipleChoiceSubject("check_ans_1");
-  var value1 = multipleChoiceSubject1.calculation();
-  var answer1 = multipleChoiceSubject.answer[0];
-  if (answer1.length == value1.length) {
-    var diffA = _.difference(value1, answer1);
-    if (_.isEmpty(diffA)) {
-      multipleChoiceSubject.scores += multipleChoiceSubject.scorePerSubject;
-    }
-  }
+	var multipleChoiceSubject1 = new MultipleChoiceSubject("check_ans_1");
+	var value1 = multipleChoiceSubject1.calculation();
+	var answer1 = multipleChoiceSubject.answer[0];
+	if (answer1.length == value1.length) {
+		var diffA = _.difference(value1, answer1);
+		if (_.isEmpty(diffA)) {
+			multipleChoiceSubject.scores += multipleChoiceSubject.scorePerSubject;
+		}
+	}
 
-  var multipleChoiceSubject2 = new MultipleChoiceSubject("check_ans_2");
-  var value2 = multipleChoiceSubject2.calculation();
-  var answer2 = multipleChoiceSubject.answer[1];
-  if (answer2.length == value2.length) {
-    var diffB = _.difference(value2, answer2);
-    if (_.isEmpty(diffB)) {
-      multipleChoiceSubject.scores += multipleChoiceSubject.scorePerSubject;
-    }
-  }
+	var multipleChoiceSubject2 = new MultipleChoiceSubject("check_ans_2");
+	var value2 = multipleChoiceSubject2.calculation();
+	var answer2 = multipleChoiceSubject.answer[1];
+	if (answer2.length == value2.length) {
+		var diffB = _.difference(value2, answer2);
+		if (_.isEmpty(diffB)) {
+			multipleChoiceSubject.scores += multipleChoiceSubject.scorePerSubject;
+		}
+	}
 
-  var multipleChoiceSubject2 = new MultipleChoiceSubject("check_ans_3");
-  var value3 = multipleChoiceSubject3.calculation();
-  var answer3 = multipleChoiceSubject.answer[3];
-  if (answer3.length == value3.length) {
-    var diffB = _.difference(value3, answer3);
-    if (_.isEmpty(diffC)) {
-      multipleChoiceSubject.scores += multipleChoiceSubject.scorePerSubject;
-    }
-  }
-  return multipleChoiceSubject.scores;
+	var multipleChoiceSubject3 = new MultipleChoiceSubject("check_ans_3");
+	var value3 = multipleChoiceSubject3.calculation();
+	var answer3 = multipleChoiceSubject.answer[2];
+	if (answer3.length == value3.length) {
+		var diffC = _.difference(value3, answer3);
+		if (_.isEmpty(diffC)) {
+			multipleChoiceSubject.scores += multipleChoiceSubject.scorePerSubject;
+		}
+	}
+	return multipleChoiceSubject.scores;
 }
 
 function trueOrFalseTopics() {
-  var trueOrFalseSubject = new Subject(
-    "trueOrFalseSubject",
-    ["no", "yes"],
-    2,
-    10
-  );
-  var trueOrFalseSubject1 = new ChoiceSubject("ans_1");
-  var value1 = trueOrFalseSubject1.calculation();
-  var trueOrFalseSubject2 = new ChoiceSubject("ans_2");
-  var value2 = trueOrFalseSubject2.calculation();
-  var value = [value1, value2];
+	var trueOrFalseSubject = new Subject("trueOrFalseSubject", ["no", "yes"], 2, 10);
+	var trueOrFalseSubject1 = new ChoiceSubject("ans_1");
+	var value1 = trueOrFalseSubject1.calculation();
+	var trueOrFalseSubject2 = new ChoiceSubject("ans_2");
+	var value2 = trueOrFalseSubject2.calculation();
+	var value = [value1, value2];
 
-  for (var i = 0; i < value.length; i++) {
-    if (value[i] == trueOrFalseSubject.answer[i]) {
-      trueOrFalseSubject.scores += trueOrFalseSubject.scorePerSubject;
-    }
-  }
-  return trueOrFalseSubject.scores;
+	for (var i = 0; i < value.length; i++) {
+		if (value[i] == trueOrFalseSubject.answer[i]) {
+			trueOrFalseSubject.scores += trueOrFalseSubject.scorePerSubject;
+		}
+	}
+	return trueOrFalseSubject.scores;
 }
 
 function shortAnswerTopics() {
-  var shortAnswerSubject = new Subject(
-    "shortAnswerSubject",
-    [
-      "Model is a simplification and abstraction of the real world, and a model is an expression of the system, process, thing or concept under study. It can be a physical entity; it can be a figure; or it can be a mathematical expression.",
-    ],
-    1,
-    20
-  );
-  var value5 = $("#short5").val();
+	var shortAnswerSubject = new Subject(
+		"shortAnswerSubject",
+		["Model is a simplification and abstraction of the real world, and a model is an expression of the system, process, thing or concept under study. It can be a physical entity; it can be a figure; or it can be a mathematical expression.",],
+		1, 20);
+	var value5 = $("#short5").val();
 
-  if (value5 == shortAnswerSubject.answer[0]) {
-    shortAnswerSubject.scores = shortAnswerSubject.scorePerSubject;
-  }
-  return shortAnswerSubject.scores;
+	if (value5 == shortAnswerSubject.answer[0]) {
+		shortAnswerSubject.scores = shortAnswerSubject.scorePerSubject;
+	}
+	return shortAnswerSubject.scores;
 }
---
+++ js/studentsList.js
@@ -0,0 +1,17 @@
+var students = {
+  1: {
+    "Ivan Ivanov": "75c191c3f1f12c13c666f5aa4c38059b",
+    "Petr Petrov": "74a7b56a3f80662ef8d5bf53d1356d3f",
+    "David Davidov": "4439ff7986b538da2a371fa666839dbe",
+  },
+  2: {
+    "Alex Alexeev": "9ecc88c0da16b336178283e47a7aa4e8",
+    "Oleg Olegov": "99eb243b7ce5a623c2eb3fc841b9b1a9",
+    "Pavel Pavlov": "28f5d5a8f2a8a0bf893b61f4f5f179d0",
+  },
+  3: {
+    "Sergey Sergeev": "4428f1596ed5e53a414ea6f020366913",
+    "Ilia Ilyin": "fa320945661c08f814d6f97cdbf36d34",
+    "Max Miximov": "39fba4eacd19ab049d828bb8fce66f2a",
+  },
+};
